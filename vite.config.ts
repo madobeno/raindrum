a499caa3f49daf726fef65bb2ee8f1ae00e91b0b
madobeno <69515239+madobeno@users.noreply.github.com>
2025-12-31 22:49:56 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
+export default define\config({ base: "/raindrum/", });
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ base: "/raindrum/", }) => {
+export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
